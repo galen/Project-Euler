2c1f068d6780c7d04f5323e429939c0a73df6584
galen <galenjr@gmail.com>
2011-07-21 14:12:29 -0400

    Add 42

---
+++ PHP/solutions/042.php
@@ -0,0 +1,24 @@
+<?php
+
+function solution_42() {
+	$data = str_getcsv( file_get_contents( 'data/042.php' ) );
+	
+	foreach( $data as $name ) {
+		foreach( str_split( $name ) as $letter ) {
+			$s += ord( $letter ) - 64;
+		}
+		if ( strpos( sqrt( 8 * $s + 1 ), '.' ) === false ) {
+			$t++;
+		}
+		$s=0;
+	}
+	return $t;
+}
+
+function answer_42() {
+	return 871198282;
+}
+
+
+
+
