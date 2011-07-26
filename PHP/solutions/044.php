472ac5d0fa66e373503cd2b2ae4473662f93f6a0
galen <galenjr@gmail.com>
2011-07-26 14:58:23 -0400

    Add #44

---
+++ PHP/solutions/044.php
@@ -0,0 +1,25 @@
+<?php
+
+function solution_44() {
+	
+	for( $i=1;$i<=5000;$i++ ) {
+		$p[] = $i*(3*$i-1)/2;
+	}
+
+	for( $j=0;$j<5000;$j++ ) {
+		for( $k=$j+1;$k<5000;$k++ ) {
+			if ( in_array( $p[$j]+$p[$k], $p ) && in_array( $p[$k]-$p[$j], $p )) {
+				return $p[$j] . ' ' . $p[$k];
+			}
+		}
+	}
+
+}
+
+function answer_44() {
+	return 5482660;
+}
+
+
+
+
